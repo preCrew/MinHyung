b3be63dc7a07257b35d1a5111ea95189f21fa159
Doosies <song961003@gmail.com>
2022-06-30 17:17:11 +0900

    Add: onChange, onKeyDown
    InputText에 값 넣고 엔터 눌렀을 때
    상태에 추가해주는거 추가해줌

--- Projects/SoloProjects/todolist/src/components/TodoList.tsx
+++ Projects/SoloProjects/todolist/src/components/TodoList.tsx
@@ -2,12 +2,15 @@ import React, {useRef, useState} from 'react';
 import InputText from './InputText';
 import Item from './Item';
 
-interface TodoListProps {
-    
+interface TList {
+    id: number;
+    text: string;
+    completed: boolean;
 }
 
 const TodoList = () => {
-    const [tasks, setTasks] = useState([
+    const [inputText, setInputText] = useState('');
+    const [tasks, setTasks] = useState<TList[]>([
         {
             id: 1,
             text: '할일 1',
@@ -26,16 +29,34 @@ const TodoList = () => {
     ]);
     const nextId = useRef(4);
 
+    // 체크박스 핸들러
     const handleClickCheckBox = (id: number) => {
-        // 해당 id를 가진 컴포넌트의 completed 상태 변경
         setTasks(tasks.map(task => 
             task.id === id ? {...task, completed: !task.completed} : task
         ));
     }
+    // 삭제버튼 핸들러
     const handleClickDeleteButton = (id: number) => {
-        // 해당 id를 가진 컴포넌트 삭제
         setTasks(tasks.filter( task => task.id !== id));
     }
+    // 입력값 변경 핸들러
+    const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    }
+    // 입력값 엔터 핸들러
+    const handleInputTextKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter'){
+            const newList: TList = {
+                id: nextId.current,
+                text: inputText,
+                completed: false,
+            }
+            setTasks(tasks.concat(newList));
+            setInputText('');
+            nextId.current += 1;
+        }
+    }
+
     return (
         <>
             {tasks.map( task => 
@@ -47,7 +68,11 @@ const TodoList = () => {
                     onClickDeleteButton={handleClickDeleteButton}
                 />
             )}
-            <InputText/>
+            <InputText
+                onChange={handleInputTextChange}
+                onKeyDown={handleInputTextKeyDown}
+                inputText={inputText}
+            />
         </>
     );
 }
